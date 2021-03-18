86017502c4a7379bf0bffeaf5e10ca1f21a5ea5b
Thomas Chardome <thomas.chardome@gmail.com>
2021-03-18 16:30:38 +0100

    add files

---
+++ src/index.php
@@ -0,0 +1,74 @@
+<?php
+//this line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+//we are going to use session variables so we need to enable sessions
+session_start();
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+//echo $_POST["email"];
+echo $_POST["street"];
+echo $_POST["streetnumber"];
+echo $_POST["city"];
+echo $_POST["zipcode"];
+$_POST["email"] = ' ';
+$_POST["street"] = ' ';
+$_POST["streetnumber"] = ' ';
+$_POST["city"] = ' ';
+$_POST["zipcode"] = ' ';
+
+
+$emailErr = "";
+$email="";
+
+if (empty($_POST["email"])) {
+    $emailErr = "* Email is required";
+} else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr= "* Invalid email format";
+    }
+}
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+//your products with their price.
+$products = [
+    ['name' => 'Margherita', 'price' => 8],
+    ['name' => 'Hawaï', 'price' => 8.5],
+    ['name' => 'Salami pepper', 'price' => 10],
+    ['name' => 'Prosciutto', 'price' => 9],
+    ['name' => 'Parmiggiana', 'price' => 9],
+    ['name' => 'Vegetarian', 'price' => 8.5],
+    ['name' => 'Four cheeses', 'price' => 10],
+    ['name' => 'Four seasons', 'price' => 10.5],
+    ['name' => 'Scampi', 'price' => 11.5]
+];
+
+$products = [
+    ['name' => 'Water', 'price' => 1.8],
+    ['name' => 'Sparkling water', 'price' => 1.8],
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 2.2],
+];
+
+$totalValue = 0;
+
+require 'form-view.php';
+
+?>
